612f7d1dc5ab63d71403a6233428340ac2042eb7
Joshb16 <stunt-josh@hotmail.com>
2014-09-23 18:09:57 +0100

    Add first project

---
+++ Joshua's/Code/First Programs/First_project/First_project/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        'code for input - output program
+        'by A. Programmer on 23/09/2014
+
+        Dim name As String
+
+        Console.Write("Enter thy name, peasant: ")
+        name = Console.ReadLine()
+        If name = "John Snow" Then
+            Console.Write("You know nothing " & name)
+        ElseIf name = "Hover Hand" Or name = "Alasdair" Then
+            Console.Write("You know you want to ;) ")
+        Else
+            Console.Write("Welcome to Isengard, " & name)
+        End If
+        Console.ReadLine()
+    End Sub
+
+End Module
